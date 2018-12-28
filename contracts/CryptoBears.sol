0e2478acdb951f404051f30a5c2714f38b90f359
Alec Schaefer <alexander_schaefer@brown.edu>
2018-12-28 18:44:01 -0500

    makes minor edits to CryptoBears contract

--- contracts/CryptoBears.sol
+++ contracts/CryptoBears.sol
@@ -23,83 +23,111 @@ contract CryptoBears is ERC721 {
   struct Bear {
     uint timeOfBirth;
     uint genes;
-    uint feedBefore;
+    uint timeLastFed;
     string name;
   }
 
-  Bear[] bears;
+  Bear[] _bears;
 
-  mapping(uint256 => mapping(uint256 => uint256)) bets;
+  mapping(uint256 => mapping(uint256 => uint256)) _bets;
 
-  uint constant startBalance = 100;
-  uint constant feedingCost = 20;
-  uint constant feedingInterval = 1 minutes;
+  uint _startBalance;
+  uint _feedingCost;
+  uint _feedingInterval;
 
-  BearBucks BearBucksContract;
+  BearBucks _BearBucksContract;
 
-  constructor() {
-    BearBucksContract = new BearBucks();
+  constructor(uint startBalance, uint feedingCost, uint feedingInterval) {
+    _BearBucksContract = new BearBucks();
+    _startBalance = startBalance;
+    _feedingCost = feedingCost;
+    _feedingInterval = feedingInterval;
   }
 
+  modifier exists(uint256 bearID) {
+    require(_exists(bearID));
+    _;
+  }
 
   function newBear(uint256 genes, address owner, string name) returns(uint) {
 
     Bear memory bear = Bear({
       timeOfBirth: now,
       genes: genes,
-      feedBefore: now + feedingInterval,
+      timeLastFed: now,
       name: name
     });
 
-    uint256 bearID = bears.push(bear) - 1;
+    uint256 bearID = _bears.push(bear) - 1;
     _mint(owner, bearID);
-    BearBucksContract.mint(owner, startBalance);
+    _BearBucksContract.mint(owner, startBalance);
     return bearID;
   }
 
-  function feed(uint256 bearID, uint256 amount) {
+  function feed(uint256 bearID, uint256 amount) exists(bearID) {
     /*Begin Solution*/
     require(msg.sender == ownerOf(bearID));
-    require(BearBucksContract.balanceOf(msg.sender) >= amount);
+    require(_BearBucksContract.balanceOf(msg.sender) >= amount);
 
-    BearBucksContract.burn(msg.sender, amount);
-    Bear storage bear = bears[bearID];
-    bear.feedBefore = now + feedingInterval;
+    uint mealsNeeded = getMealsNeeded(bearID);
+    require(mealsNeeded > 0);
+
+    uint mealsFed = amount.div(_feedingCost);
+    if (mealsFed > mealsNeeded) {
+      mealsFed = mealsNeeded;
+    }
+    _BearBucksContract.burn(msg.sender, mealsFed.mul(_feedingCost));
+    Bear storage bear = _bears[bearID];
+    bear.timeLastFed = bear.timeLastFed.add(mealsFed.mul(_feedingInterval));
     emit bearFed(bearID);
     /*End Solution*/
   }
 
-  function placeBet(uint256 bearID, uint256 opponentID, uint256 amount) {
+  function getMealsNeeded(uint256 bearID)
+    exists(bearID) view returns(uint)
+  {
+    Bear bear = _bears[bearID];
+    uint timeSinceLastFed = now.sub(bear.timeLastFed);
+    return timeSinceLastFed.div(_feedingInterval);
+  }
+
+  function placeBet(uint256 bearID, uint256 opponentID, uint256 amount)
+    exists(bearID) exists(opponentID)
+  {
     /*Begin Solution*/
     require(msg.sender == ownerOf(bearID));
     require(amount > 0);
-    require(bets[bearID][opponentID] == 0);
+    require(_bets[bearID][opponentID] == 0);
 
-    BearBucksContract.placeBet(msg.sender, amount);
-    bets[bearID][opponentID] = amount;
+    _BearBucksContract.placeBet(msg.sender, amount);
+    _bets[bearID][opponentID] = amount;
 
     emit betPlaced(bearID, opponentID, amount);
     /*End Solution*/
   }
 
-  function removeBet(uint256 bearID, uint256 opponentID) {
+  function removeBet(uint256 bearID, uint256 opponentID)
+    exists(bearID) exists(opponentID)
+  {
     /*Begin Solution*/
     require(msg.sender == ownerOf(bearID));
-    require(bets[bearID][opponentID] > 0);
+    require(_bets[bearID][opponentID] > 0);
 
-    BearBucksContract.removeBet(ownerOf(bearID), bets[bearID][opponentID]);
-    bets[bearID][opponentID] = 0;
+    _BearBucksContract.removeBet(ownerOf(bearID), _bets[bearID][opponentID]);
+    _bets[bearID][opponentID] = 0;
 
     emit betRemoved(bearID, opponentID);
     /*End Solution*/
   }
 
-  function payWinner(uint256 winner, uint256 loser) external {
+  function payWinner(uint256 winner, uint256 loser)
+    exists(winner) exists(loser) external
+  {
     /*Begin Solution*/
-    require(bets[winner][loser] > 0 && bets[loser][winner] > 0);
+    require(_bets[winner][loser] > 0 && _bets[loser][winner] > 0);
 
-    BearBucksContract.transferFrom(
-      ownerOf(loser), ownerOf(winner), bets[loser][winner]
+    _BearBucksContract.transferFrom(
+      ownerOf(loser), ownerOf(winner), _bets[loser][winner]
     );
     removeBet(winner, loser);
     removeBet(loser, winner);
