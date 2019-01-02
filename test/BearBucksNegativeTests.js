4c675c5d2d849aceea2b1e3c604ad287e7161401
Alec Schaefer <alexander_schaefer@brown.edu>
2019-01-01 19:05:32 -0500

    finishes BearBucks negative tests

--- test/BearBucksNegativeTests.js
+++ test/BearBucksNegativeTests.js
@@ -123,48 +123,76 @@ contract('BearBucksNegativeTests', async function (accounts) {
     await checkState([bearBucks], [stateChanges], accounts)
   })
 
-  //TODO: finish tests below 
-  // it('should fail to placeBet if not CryptoBearsContract', async function () {
-  //
-  //   var stateChanges = [
-  //     {'var': '', 'expect': }
-  //   ]
-  //   await checkState([bearBucks], [stateChanges], accounts)
-  // })
-  //
-  // /*NOTE: This test gives away part of the solution*/
-  // it('should fail to placeBet greater than balance', async function () {
-  //
-  //   var stateChanges = [
-  //     {'var': '', 'expect': }
-  //   ]
-  //   await checkState([bearBucks], [stateChanges], accounts)
-  // })
-  //
-  // /*NOTE: This test gives away part of the solution*/
-  // it('should fail to placeBet if allowance of CryptoBearsContract is less than betSum', async function () {
-  //
-  //   var stateChanges = [
-  //     {'var': '', 'expect': }
-  //   ]
-  //   await checkState([bearBucks], [stateChanges], accounts)
-  // })
-  //
-  // it('should fail to removeBet if not CryptoBearsContract', async function () {
-  //
-  //   var stateChanges = [
-  //     {'var': '', 'expect': }
-  //   ]
-  //   await checkState([bearBucks], [stateChanges], accounts)
-  // })
-  //
-  // /*NOTE: This test gives away part of the solution*/
-  // it('should fail to approve amount less than betSum for CryptoBearsContract', async function () {
-  //
-  //   var stateChanges = [
-  //     {'var': '', 'expect': }
-  //   ]
-  //   await checkState([bearBucks], [stateChanges], accounts)
-  // })
+  it('should fail to placeBet if not CryptoBearsContract', async function () {
+    await bearBucks.mint(accounts[1], amount)
+    await bearBucks.approve(accounts[0], amount, {from: accounts[1]})
+    await expectRevert(bearBucks.placeBet(accounts[1], amount, {from: accounts[3]}))
+
+    var stateChanges = [
+      {'var': 'totalSupply', 'expect': amount},
+      {'var': 'balanceOf.a1', 'expect': amount},
+      {'var': 'allowance.a1.a0', 'expect': amount}
+    ]
+    await checkState([bearBucks], [stateChanges], accounts)
+  })
+
+  /*NOTE: This test gives away part of the solution*/
+  it('should fail to placeBet greater than balance', async function () {
+    await bearBucks.mint(accounts[1], amount)
+    await bearBucks.approve(accounts[0], amount+1, {from: accounts[1]})
+    await expectRevert(bearBucks.placeBet(accounts[1], amount+1, {from: accounts[0]}))
+
+    var stateChanges = [
+      {'var': 'totalSupply', 'expect': amount},
+      {'var': 'balanceOf.a1', 'expect': amount},
+      {'var': 'allowance.a1.a0', 'expect': amount+1}
+    ]
+    await checkState([bearBucks], [stateChanges], accounts)
+  })
+
+  /*NOTE: This test gives away part of the solution*/
+  it('should fail to placeBet if allowance of CryptoBearsContract is less than betSum', async function () {
+    await bearBucks.mint(accounts[1], amount)
+    await bearBucks.approve(accounts[0], amount-1, {from: accounts[1]})
+    await expectRevert(bearBucks.placeBet(accounts[1], amount, {from: accounts[0]}))
+
+    var stateChanges = [
+      {'var': 'totalSupply', 'expect': amount},
+      {'var': 'balanceOf.a1', 'expect': amount},
+      {'var': 'allowance.a1.a0', 'expect': amount-1}
+    ]
+    await checkState([bearBucks], [stateChanges], accounts)
+  })
+
+  it('should fail to removeBet if not CryptoBearsContract', async function () {
+    await bearBucks.mint(accounts[1], amount)
+    await bearBucks.approve(accounts[0], amount, {from: accounts[1]})
+    await bearBucks.placeBet(accounts[1], amount, {from: accounts[0]})
+    await expectRevert(bearBucks.removeBet(accounts[1], amount, {from: accounts[3]}))
+
+    var stateChanges = [
+      {'var': 'totalSupply', 'expect': amount},
+      {'var': 'balanceOf.a1', 'expect': amount},
+      {'var': 'allowance.a1.a0', 'expect': amount},
+      {'var': 'betSum.a1', 'expect': amount}
+    ]
+    await checkState([bearBucks], [stateChanges], accounts)
+  })
+
+  /*NOTE: This test gives away part of the solution*/
+  it('should fail to approve amount less than betSum for CryptoBearsContract', async function () {
+    await bearBucks.mint(accounts[1], amount)
+    await bearBucks.approve(accounts[0], amount, {from: accounts[1]})
+    await bearBucks.placeBet(accounts[1], amount, {from: accounts[0]})
+    await expectRevert(bearBucks.approve(accounts[0], amount-1, {from: accounts[1]}))
+
+    var stateChanges = [
+      {'var': 'totalSupply', 'expect': amount},
+      {'var': 'balanceOf.a1', 'expect': amount},
+      {'var': 'allowance.a1.a0', 'expect': amount},
+      {'var': 'betSum.a1', 'expect': amount}
+    ]
+    await checkState([bearBucks], [stateChanges], accounts)
+  })
 
 })
